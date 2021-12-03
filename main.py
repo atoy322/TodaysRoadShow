46d80b37b35a26dae11ad7b0e72f7a2a04874781
atoy322 <atoy_0322@yahoo.co.jp>
2021-12-04 00:19:39 +0900

    improve code style

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import requests
 from color import cprint
 
 
+
+URL = "https://kinro.ntv.co.jp/lineup"
+SEARCH_PATTERN = '<div class="title">.+?</div>'
+
 friday = 4
 weekdays = ["月", "火", "水", "木", "金", "土", "日"]
 
@@ -14,11 +18,8 @@ if weekday != friday:
     print(f"今日は{weekdays[weekday]}曜日です")
     exit()
 
-URL = "https://kinro.ntv.co.jp/lineup"
-SEARCH_PATTERN = '<div class="title">.+?</div>'
 
 content = requests.get(URL).content.decode()
-
 div = re.findall(SEARCH_PATTERN, content)[0]
 title = re.sub("<.+?>", "", div)
 
